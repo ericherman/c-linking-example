55eae39e79854a91c2cf018f54f674f99ffb0bbb
Eric Herman <eric@freesa.org>
2018-11-03 10:50:11 +0100

    LGPLv2.1+ boilerplate

--- static-local-makefile/ctest1.c
+++ static-local-makefile/ctest1.c
@@ -1,4 +1,19 @@
- void ctest1(int *i)
+/* ctest1.c
+   Copyright (C) 2013, 2018 Eric Herman <eric@freesa.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+	https://www.gnu.org/licenses/lgpl-2.1.txt
+ */
+void ctest1(int *i)
 {
 	*i = 5;
 }
